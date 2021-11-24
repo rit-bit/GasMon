0647c5b88e0dee14083ab30f18bae5e4bf3dcaad
Richard Sutch <richard.sutch@softwire.com>
2021-11-24 15:01:58 +0000

    Initial commit

---
+++ GasMon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GasMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
